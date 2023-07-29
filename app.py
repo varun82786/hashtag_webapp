b54bb9a3af89fa60d51df2c02ccbbf24c2b4a237
varun82786 <varunsiddartha82786@gmail.com>
2023-07-29 15:41:28 +0530

    file migration

--- app.py
+++ app.py
@@ -1,45 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
-import json
-import random
-import string
+
+#user defined libs
+from scripts.operationsAPI import operationsAPI 
+from scripts.mongoAPI import mongoAPI
+
 
 app = Flask(__name__)
-
-# Define JSON file paths as global variables
-CREDENTIALS_JSON_PATH= r'database/authorization/auth/auth.json'
-HASHTAG_JSON_PATH  = r'database/source_data/street_photography/hashtagDB.json'
-
-def generate_random_string(length):
-    # Define the characters to choose from
-    characters = string.ascii_letters + string.digits
-
-    # Generate the random string
-    random_string = ''.join(random.choice(characters) for _ in range(length))
-
-    return random_string
-
-# Load hashtags data from JSON file
-def load_hashtags_data():
-    with open(HASHTAG_JSON_PATH) as file:
-        hashtags_data = [hashtag for hashtag in json.load(file)]
-    return hashtags_data
-
-# Load credentials data from JSON file
-def load_credentials_data():
-    with open(CREDENTIALS_JSON_PATH) as file:
-        credentials_data = json.load(file)
-    return credentials_data
-
-# Update credentials data in JSON file
-def update_credentials_data(credentials_data):
-    existing_credentials_data = load_credentials_data()
-
-    # Append new data to existing credentials object
-    existing_credentials_data.update(credentials_data)
-
-    # Update credentials JSON file
-    with open(CREDENTIALS_JSON_PATH, 'w') as file:
-        json.dump(existing_credentials_data, file, indent=4)
 
 # Landing page - Signup or Login
 @app.route('/')
@@ -51,7 +17,7 @@ def landing():
 def signup():
     if request.method == 'POST':
         # Process signup form data and update credentials
-        credentials_data = load_credentials_data()
+        credentials_data = operationsAPI.load_credentials_data()
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
@@ -61,7 +27,7 @@ def signup():
             return render_template('signup.html', error='Username or email already exists.')
 
         # Add new user credentials
-        user_id = generate_random_string(10) 
+        user_id = operationsAPI.generate_random_string(10) 
         user_no=str(len(credentials_data) + 1)
         credentials_data[username] = {
             'unique_id': user_id,
@@ -73,7 +39,7 @@ def signup():
         }
         print(credentials_data)
         # Update credentials JSON file
-        update_credentials_data(credentials_data)
+        operationsAPI.update_credentials_data(credentials_data)
 
         # Redirect to login page
         return redirect(url_for('login'))
@@ -85,7 +51,7 @@ def signup():
 def login():
     if request.method == 'POST':
         # Process login form data and authenticate user
-        credentials_data = load_credentials_data()
+        credentials_data = operationsAPI.load_credentials_data()
         username = request.form['username']
         password = request.form['password']
 
@@ -104,7 +70,7 @@ def login():
 def generator():
     if request.method == 'POST':
         # Process hashtag generation form data
-        hashtags_data = load_hashtags_data()
+        hashtags_data = operationsAPI.load_hashtags_data()
         hashtags = request.form['hashtags']
         generated_hashtags = []
 
@@ -112,12 +78,8 @@ def generator():
         input_hashtags = [tag.strip() for tag in hashtags.split(',')]
 
         # Generate hashtags based on input
-        for input_tag in input_hashtags:
-            for data_tag in hashtags_data:
-                if input_tag.lower() in data_tag.lower():# or data_tag.lower() in input_tag.lower():
-                    generated_hashtags.append("#"+data_tag)
-                    #break  # Break out of inner loop if a match is found
-        #print(generated_hashtags)
+        generated_hashtags = operationsAPI.generate_hashtags(input_hashtags,hashtags_data)
+        
         if generated_hashtags:
             return render_template('generator.html', input_hashtags=input_hashtags, generated_hashtags=generated_hashtags)
         else:
@@ -125,5 +87,5 @@ def generator():
 
     return render_template('generator.html')
 
-#if __name__ == '__main__':
-#     app.run(host="0.0.0.0",port=80,debug=True)
+if __name__ == '__main__':
+     app.run(host="0.0.0.0",port=80,debug=True)
---
+++ scripts/operationsAPI/operationsAPI.py
@@ -0,0 +1,54 @@
+import json
+import random
+import string
+
+
+# Define JSON file paths as global variables
+CREDENTIALS_JSON_PATH= r'database/authorization/auth/auth.json'
+HASHTAG_JSON_PATH  = r'database/source_data/street_photography/hashtagDB.json'
+
+def generate_random_string(length):
+    # Define the characters to choose from
+    characters = string.ascii_letters + string.digits
+
+    # Generate the random string
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+
+    return random_string
+
+# Load hashtags data from JSON file
+def load_hashtags_data():
+    with open(HASHTAG_JSON_PATH) as file:
+        hashtags_data = [hashtag for hashtag in json.load(file)]
+    return hashtags_data
+
+# Load credentials data from JSON file
+def load_credentials_data():
+    with open(CREDENTIALS_JSON_PATH) as file:
+        credentials_data = json.load(file)
+    return credentials_data
+
+# Update credentials data in JSON file
+def update_credentials_data(credentials_data):
+    existing_credentials_data = load_credentials_data()
+
+    # Append new data to existing credentials object
+    existing_credentials_data.update(credentials_data)
+
+    # Update credentials JSON file
+    with open(CREDENTIALS_JSON_PATH, 'w') as file:
+        json.dump(existing_credentials_data, file, indent=4)
+
+## Generate hashtags based on input and return the random of the hashtag list    
+def generate_hashtags(input_hashtags, hashtags_data):
+    generated_hashtags = []
+
+    for input_tag in input_hashtags:
+        for data_tag in hashtags_data:
+            if input_tag.lower() in data_tag.lower():
+                generated_hashtags.append("#" + data_tag)
+
+    # Shuffle the generated hashtags randomly
+    random.shuffle(generated_hashtags)
+
+    return generated_hashtags
