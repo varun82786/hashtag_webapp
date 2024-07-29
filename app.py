f407e5e13b0daf2d32be679ead7ba995a611a1c2
siddartha <varunsiddartha82786@gmail.com>
2024-07-30 00:59:40 +0530

    for test on rpi

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ def generator():
     return render_template('generator.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", debug=True)
